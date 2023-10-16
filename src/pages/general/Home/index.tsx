6638da3dba62fe15c697aa3320c920345fc47b7d
RafaelSI20 <148152363+RafaelSI20@users.noreply.github.com>
2023-10-16 18:03:28 -0400

    Update index.tsx

--- src/pages/general/Home/index.tsx
+++ src/pages/general/Home/index.tsx
@@ -5,8 +5,9 @@ export function Home() {
     w-full
     h-full
     flex
-    flex-row
-    border-orange-500
+    flex-column
+    p-2
+    border-blue-500
     surface-overlay
     border-3
     border-round
@@ -14,11 +15,60 @@ export function Home() {
     align-items-center
     justify-items-center
     `
+    const miniBlue = `
+        border-blue-500
+        border-3
+        border-round
+        h-4rem
+        w-8rem
+        text-align-center
+    `
+    const firstRow=`
+        flex
+        flex-row
+        w-full
+        align-items-center
+        justify-content-between
+    `
+    const secondRow=`
+        flex
+        flex-row
+        w-full
+        justify-content-evenly
+    `
+    
+    const miniGreen=`
+        border-3
+        border-green-500
+        border-round
+        align-items-center
+        h-4rem
+        w-8rem
+        m-2
+    `
 
+    const thirdRow=`
+        flex    
+        justify-content-center
+        align-item-center
+    `
     return (
         <>
             <div className={`w-full h-screen flex flex-row m-2 p-4`}>
-                <div className={style}>2</div>
+                <div className={style}>
+                    <div className={firstRow}>    
+                        <div className={miniBlue}>1</div>
+                        <div className={miniBlue}>2</div>
+                    </div>
+                    <div className={secondRow}>
+                        <div className={miniGreen}>1</div>
+                        <div className={miniGreen}>2</div>
+                        <div className={miniGreen}>3</div>
+                    </div>
+                    <div className={thirdRow}>
+                        <div className={miniGreen}>1</div>
+                    </div>
+                </div>
                 <div className={style}>1</div>
             </div>
         </>
