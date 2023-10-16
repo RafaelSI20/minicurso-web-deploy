caf82c706f66d0d4fd0314ac528c4f842c3c75d7
Carlos Hagge <carloshaggecunha@gmail.com>
2023-10-16 17:14:59 -0400

    add styles to div

--- src/pages/general/Home/index.tsx
+++ src/pages/general/Home/index.tsx
@@ -1,13 +1,25 @@
 import {useAuth} from "@/provider/Auth";
 
 export function Home() {
-    const {perfil} = useAuth();
+    const style= `
+    w-full
+    h-full
+    flex
+    flex-row
+    border-orange-500
+    surface-overlay
+    border-3
+    border-round
+    font-bold m-2
+    align-items-center
+    justify-items-center
+    `
 
     return (
         <>
-            <div className="outer">
-                <div>1</div>
-                <div>2</div>
+            <div className={`w-full h-screen flex flex-row m-2 p-4`}>
+                <div className={style}>2</div>
+                <div className={style}>1</div>
             </div>
         </>
     )
